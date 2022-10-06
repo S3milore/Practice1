a9f0159d958add81325c1eb28fdd06d846a90259
Your Name <semiloreawolusi@gmail.com>
2022-10-06 06:10:56 -0400

    link to 'test' code

---
+++ main.js
@@ -0,0 +1,20 @@
+let arr1=[];
+function nameFunc(val){
+let input=val;
+  for (let i=0; i<input.length;i++){
+    arr1.push(input[i]);}
+    return arr1;
+}
+console.log(nameFunc("Paul"));
+
+/**let arr=[];
+function nameSpace(val) {
+  if(val=="a"){
+  arr.push(1);
+  }else if (val=="b"){
+    arr.push(2);}git config --global user.name "Your Name"
+    return arr;
+}https://jsfiddle.net/S3milore/mhgks4v3/72/
+  // Only change code above this line
+console.log(nameSpace("a"));
+*/
